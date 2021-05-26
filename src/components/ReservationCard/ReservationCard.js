734688e4e893732f9907a2902b9df6411a1f8353
Riley Martin <riley.willow@gmail.com>
2021-05-26 09:43:30 -0600

    Return reservation details in ReservationCard component

--- src/components/ReservationCard/ReservationCard.js
+++ src/components/ReservationCard/ReservationCard.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-const ReservationCard = () => {
+const ReservationCard = ({ id, name, date, time, number, cancelReservation }) => {
+  return (
+    <article>
+      <h3>{name}</h3>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>{number}</p>
+      <button onClick={cancelReservation()}>Cancel</button>
+    </article>
+  )
   //needs to accept cancelReservation method as props and have it on a cancel button
   //needs to accept all relevant data for reservations and display it
 }
